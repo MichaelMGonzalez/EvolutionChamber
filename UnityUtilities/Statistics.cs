d4052c6b842b7f3448a1618955fb553ab95b0c36
MichaelMGonzalez <mmg005@ucsd.edu>
2016-09-18 21:07:45 -0700

    Cleaning directory issues

--- UnityUtilities/Statistics.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class Statistics  {
-    public static float GaussConstant(float sigma)
-    {
-        return 1.0f / (Mathf.Sqrt(2 * sigma * sigma * Mathf.PI));
-    }
-}
